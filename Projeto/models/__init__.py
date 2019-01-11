6e67ef88fa263c7cf820bb20d2f6042387d74d3d
Danidanidoo <danibrendo@gmail.com>
2019-01-11 17:41:13 +0000

    Teste

--- Projeto/models/__init__.py
+++ Projeto/models/__init__.py
@@ -4,10 +4,11 @@ except IOError:
     file = open('Login.txt','w+')
 
 Login_DB = file.readlines()
-
+'''
 print (Login_DB)
 for linha in Login_DB:
     print(linha)
     Palavras = linha.split("|")
     print (Palavras)
 input()
+'''
